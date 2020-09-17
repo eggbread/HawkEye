1858053849771bacf63fa6e4db01b96ea1f725e9
“eggbread” <gyeongmin251@gmail.com>
2020-09-18 01:14:08 +0900

    Change to yolov4

--- deep_sort/__init__.py
+++ deep_sort/__init__.py
@@ -1,1 +1,21 @@
-# vim: expandtab:ts=4:sw=4
+from .deep_sort import DeepSort
+
+
+__all__ = ['DeepSort', 'build_tracker']
+
+
+def build_tracker(cfg, use_cuda):
+    return DeepSort(cfg['DEEPSORT']['REID_CKPT'],
+                max_dist=float(cfg['DEEPSORT']['MAX_DIST']), min_confidence=float(cfg['DEEPSORT']['MIN_CONFIDENCE']),
+                nms_max_overlap=float(cfg['DEEPSORT']['NMS_MAX_OVERLAP']), max_iou_distance=float(cfg['DEEPSORT']['MAX_IOU_DISTANCE']),
+                max_age=int(cfg['DEEPSORT']['MAX_AGE']), n_init=int(cfg['DEEPSORT']['N_INIT']), nn_budget=int(cfg['DEEPSORT']['NN_BUDGET']), use_cuda=use_cuda)
+    
+
+
+
+
+
+
+
+
+
